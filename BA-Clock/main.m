a15500caaba6d94f25a39f51d31d665a72016914
roberto ramirez <robertoramirez@robertos-Mac-mini.local>
2013-11-11 16:03:30 +0800

    Initial Commit

---
+++ BA-Clock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BA-Clock
+//
+//  Created by roberto ramirez on 11/11/13.
+//  Copyright (c) 2013 lovetthomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
